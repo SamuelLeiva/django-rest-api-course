30bb55d0be7b1a04f6b1f5e81ab05b324d6c6676
SamuelLeiva <leiva20152@gmail.com>
2022-05-18 15:25:55 -0500

    DRF ListCreateView

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -18,7 +18,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
         serializer.save(content = content)
         # send a django signal (revisar curso anterior)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -26,3 +26,12 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup_filed = 'pk'
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+    """ 
+    Not gonna use this method
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
