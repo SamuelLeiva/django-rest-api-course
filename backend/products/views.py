be785f3516822b9c42be36070df83c0573ed5445
SamuelLeiva <leiva20152@gmail.com>
2022-05-19 16:15:53 -0500

    Add custom permission

--- backend/products/views.py
+++ backend/products/views.py
@@ -7,12 +7,13 @@ from yaml import serialize
 
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsStaffEditorPermission
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer): # modificar la creacion
         # serializer.save(user=self.request.user)
